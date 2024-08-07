91bd28e16d0f5699b1d0bfdc9c891a7fbe7b0a5a
Hai Thanh <haithanh.nguyen1001@gmail.com>
2024-08-07 17:59:06 +0700

    login responsive page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,40 @@
-
+import "./Login.scss";
 const Login = (props) => {
   return (
     <div className="login-container">
-      Login
-      <button className="btn btn-primary">Test bootstrap</button>
+      <div className="container">
+        <div className="row px-3 px-sm-0">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block">
+            <div className="brand">Company ABC</div>
+            <div className="detail">Connect and manage your company</div>
+          </div>
+          <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3">
+            <div className="brand d-sm-none">Company ABC</div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email address or your phone number"
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+            <button className="btn btn-primary">Login</button>
+            <span className="text-center">
+              <a className="forgot-password" href="#">
+                Forgot your password?
+              </a>
+            </span>
+            <hr />
+            <div className="text-center">
+              <button className="btn btn-success">Create new account</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
